2999b31318ad986df309677e98d71ee9b4a47486
Ronald Blüthl <ronald.bluethl@devoted.dev>
2022-04-05 14:09:33 +0200

    Return version from status endpoints

--- pages/api/v2/status/index.ts
+++ pages/api/v2/status/index.ts
@@ -4,7 +4,9 @@ import { methodNotAllowed } from 'models/error'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // GET /api/v2/status
   if (req.method === 'GET') {
-    return res.status(200).end()
+    return res.status(200).json({
+      version: 'v2'
+    })
   }
 
   return res.status(405).end(methodNotAllowed)
